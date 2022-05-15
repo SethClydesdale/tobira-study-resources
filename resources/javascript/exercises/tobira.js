5600a50247cd713b82dea6b983f789f07f4f423c
Seth Clydesdale <sethc1995@gmail.com>
2022-05-15 11:38:38 -0400

    add new exercises, grammar points
    - added grammar points 1-4 to the grammar index for lesson 3.
    - added the following exercises.
    
    # 第3課
    - 単語：カタカナ語

--- resources/javascript/exercises/tobira.js
+++ resources/javascript/exercises/tobira.js
@@ -117,6 +117,7 @@ window.GenkiExercises = [
   'lesson-3/vocab-5|単語：会話文・その2|p.63',
   'lesson-3/kanji-1|漢字：読み方を書く・その1|p.74',
   'lesson-3/kanji-2|漢字：読み方を書く・その2|p.74',
+  'lesson-3/vocab-6|単語：カタカナ語|p.75',
 
   // Appendix
   'appendix/vocabulary-index|単語索引|p.362-398',
