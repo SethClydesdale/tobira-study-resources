14bfefcae2008d1ea02d9e34116d7f523485c71a
Seth Clydesdale <sethc1995@gmail.com>
2022-05-18 12:08:26 -0400

    add new exercises, grammar points
    - added grammar points 13-16 to the grammar index for lesson 3.
    - added the following exercises.
    
    # 第3課
    - 基礎練習：問題1

--- resources/javascript/exercises/tobira.js
+++ resources/javascript/exercises/tobira.js
@@ -118,6 +118,7 @@ window.GenkiExercises = [
   'lesson-3/kanji-1|漢字：読み方を書く・その1|p.74',
   'lesson-3/kanji-2|漢字：読み方を書く・その2|p.74',
   'lesson-3/vocab-6|単語：カタカナ語|p.75',
+  'lesson-3/grammar-wb-1|基礎練習：問題1|p.29',
 
   // Appendix
   'appendix/vocabulary-index|単語索引|p.362-398',
