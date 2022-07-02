ab6d5de24826fd5ae1157ca8ac0647c553edd245
Seth Clydesdale <sethc1995@gmail.com>
2022-07-02 10:44:23 -0400

    add new exercises
    - added the following exercises.
    
    # 第5課
    - 基礎練習：問題2

--- resources/javascript/exercises/tobira.js
+++ resources/javascript/exercises/tobira.js
@@ -208,6 +208,7 @@ window.GenkiExercises = [
   'lesson-5/kanji-1|漢字：読み方を書く・その1|p.124',
   'lesson-5/kanji-2|漢字：読み方を書く・その2|p.124',
   'lesson-5/grammar-wb-1|基礎練習：問題1|p.57',
+  'lesson-5/grammar-wb-2|基礎練習：問題2|p.57-58',
 
   // Appendix
   'appendix/vocabulary-index|単語索引|p.362-398',
