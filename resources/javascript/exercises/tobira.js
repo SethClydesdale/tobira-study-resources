3fe275d390e4c2be41eb1fa55d38d4be1619ec32
Seth Clydesdale <sethc1995@gmail.com>
2022-07-18 11:03:20 -0400

    add new exercises, fix typos
    - fixed typos in the grammar index; wise --> wish, I hop --> I hope. (wtf was I doing here)
    - added the following exercises.
    
    # 第5課
    - 漢字の練習：問題6
    - 漢字の練習：問題7

--- resources/javascript/exercises/tobira.js
+++ resources/javascript/exercises/tobira.js
@@ -238,6 +238,8 @@ window.GenkiExercises = [
   'lesson-5/kanji-wb-6|漢字の練習：問題3|p.77',
   'lesson-5/kanji-wb-7|漢字の練習：問題4|p.77',
   'lesson-5/kanji-wb-8|漢字の練習：問題5|p.78',
+  'lesson-5/kanji-wb-9|漢字の練習：問題6|p.78',
+  'lesson-5/kanji-wb-10|漢字の練習：問題7|p.79',
 
   // Appendix
   'appendix/vocabulary-index|単語索引|p.362-398',
