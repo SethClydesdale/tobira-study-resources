47698492c6a3c7c66e14dabd09ea9ef86480b6ba
Seth Clydesdale <sethc1995@gmail.com>
2022-08-03 11:49:45 -0400

    add new exercises, hover to view furigana, update break timer
    - updated break timer with automatic time incrementation based on number of break sessions (max 60min or 8 breaks).
    - added hover to view furigana/reading for multi-choice vocab exercises when furigana is hidden.
    - added the following exercises.
    
    # 第6課
    - 基礎練習：問題15

--- resources/javascript/exercises/tobira.js
+++ resources/javascript/exercises/tobira.js
@@ -265,6 +265,7 @@ window.GenkiExercises = [
   'lesson-6/grammar-wb-12|基礎練習：問題12|p.77-78',
   'lesson-6/grammar-wb-13|基礎練習：問題13|p.78',
   'lesson-6/grammar-wb-14|基礎練習：問題14|p.79',
+  'lesson-6/grammar-wb-15|基礎練習：問題15|p.79',
 
   // Appendix
   'appendix/vocabulary-index|単語索引|p.362-398',
