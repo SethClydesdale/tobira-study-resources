2e7003501b185d6966352c6be02ce4b693d1a4e4
Seth Clydesdale <sethc1995@gmail.com>
2022-08-13 10:38:14 -0400

    add new exercises
    - added the following exercises.
    
    # 第7課
    - 単語：読み物1・その2
    - 単語：読み物1・その3
    - 単語：読み物2
    - 単語：会話文・その1
    - 単語：会話文・その2
    - 漢字：読み方を書く・その1
    - 漢字：読み方を書く・その2

--- resources/javascript/exercises/tobira.js
+++ resources/javascript/exercises/tobira.js
@@ -290,6 +290,13 @@ window.GenkiExercises = [
 
   // Lesson 7
   'lesson-7/vocab-1|単語：読み物1・その1|p.155',
+  'lesson-7/vocab-2|単語：読み物1・その2|p.155',
+  'lesson-7/vocab-3|単語：読み物1・その3|p.155-156',
+  'lesson-7/vocab-4|単語：読み物2|p.162',
+  'lesson-7/vocab-5|単語：会話文・その1|p.165',
+  'lesson-7/vocab-6|単語：会話文・その2|p.165',
+  'lesson-7/kanji-1|漢字：読み方を書く・その1|p.175',
+  'lesson-7/kanji-2|漢字：読み方を書く・その2|p.175',
 
   // Appendix
   'appendix/vocabulary-index|単語索引|p.362-398',
