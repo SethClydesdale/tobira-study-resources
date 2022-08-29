965339d00ec683d0e59a8fea68296d2541dd6a3f
Seth Clydesdale <sethc1995@gmail.com>
2022-08-29 11:17:25 -0400

    add new exercises
    - added the following exercises.
    
    # 第7課
    - 応用練習：問題1
    - 応用練習：問題2
    - 応用練習：問題3

--- resources/javascript/exercises/tobira.js
+++ resources/javascript/exercises/tobira.js
@@ -314,6 +314,9 @@ window.GenkiExercises = [
   'lesson-7/grammar-wb-15|基礎練習：問題15|p.93',
   'lesson-7/grammar-wb-16|基礎練習：問題16|p.94',
   'lesson-7/grammar-wb-17|基礎練習：問題17|p.94',
+  'lesson-7/grammar-wb-18|応用練習：問題1|p.95',
+  'lesson-7/grammar-wb-19|応用練習：問題2|p.96',
+  'lesson-7/grammar-wb-20|応用練習：問題3|p.97-98',
 
   // Appendix
   'appendix/vocabulary-index|単語索引|p.362-398',
