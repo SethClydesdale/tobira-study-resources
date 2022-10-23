93c7d70c92a9407ceec8215cc7f5c8ebadeb3ec3
Seth Clydesdale <sethc1995@gmail.com>
2022-10-23 10:20:03 -0400

    add new exercises
    - added the following exercises.
    
    # 第9課
    - 漢字の練習：問題5

--- resources/javascript/exercises/tobira.js
+++ resources/javascript/exercises/tobira.js
@@ -422,6 +422,7 @@ window.GenkiExercises = [
   'lesson-9/kanji-wb-5|漢字の練習：問題2|p.129-130',
   'lesson-9/kanji-wb-6|漢字の練習：問題3|p.130',
   'lesson-9/kanji-wb-7|漢字の練習：問題4|p.131',
+  'lesson-9/kanji-wb-8|漢字の練習：問題5|p.132',
   
   // Appendix
   'appendix/vocabulary-index|単語索引|p.362-398',
