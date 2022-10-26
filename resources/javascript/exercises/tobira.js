4ae0a9ff8415959e7ff1629de570b82385ded610
Seth Clydesdale <sethc1995@gmail.com>
2022-10-26 12:03:26 -0400

    add new exercises, fix typo
    - fixed typo in lesson 10 vocab; おしゃべる --> おしゃべり
    - added the following exercises.
    
    # 第10課
    - 単語：読み物・その2
    - 単語：読み物・その3
    - 単語：会話文
    - 漢字：読み方を書く・その1
    - 漢字：読み方を書く・その2

--- resources/javascript/exercises/tobira.js
+++ resources/javascript/exercises/tobira.js
@@ -429,6 +429,11 @@ window.GenkiExercises = [
 
   // Lesson 10
   'lesson-10/vocab-1|単語：読み物・その1|p.227',
+  'lesson-10/vocab-2|単語：読み物・その2|p.227-228',
+  'lesson-10/vocab-3|単語：読み物・その3|p.228',
+  'lesson-10/vocab-4|単語：会話文|p.231',
+  'lesson-10/kanji-1|漢字：読み方を書く・その1|p.241',
+  'lesson-10/kanji-2|漢字：読み方を書く・その2|p.241',
   
   // Appendix
   'appendix/vocabulary-index|単語索引|p.362-398',
