a8162fc110cb47035f8703bc7688f713b2a22916
Seth Clydesdale <sethc1995@gmail.com>
2022-10-31 11:13:06 -0400

    add new exercises, definitions
    - added new definitions to the dictionary.
    - added the following exercises.
    
    # 第10課
    - 基礎練習：問題2

--- resources/javascript/exercises/tobira.js
+++ resources/javascript/exercises/tobira.js
@@ -435,6 +435,7 @@ window.GenkiExercises = [
   'lesson-10/kanji-1|漢字：読み方を書く・その1|p.241',
   'lesson-10/kanji-2|漢字：読み方を書く・その2|p.241',
   'lesson-10/grammar-wb-1|基礎練習：問題1|p.135',
+  'lesson-10/grammar-wb-2|基礎練習：問題2|p.135-136; A, B, & C',
   
   // Appendix
   'appendix/vocabulary-index|単語索引|p.362-398',
