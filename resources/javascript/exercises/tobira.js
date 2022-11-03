20d55c46c1d95e76197d2857ebb3e8774ead734e
Seth Clydesdale <sethc1995@gmail.com>
2022-11-03 09:40:09 -0400

    add new exercises, fixes
    - adjust problem width on yesterday's exercises.
      - also adds missing kana only answer.
    - added the following exercises.
    
    # 第10課
    - 基礎練習：問題7

--- resources/javascript/exercises/tobira.js
+++ resources/javascript/exercises/tobira.js
@@ -440,6 +440,7 @@ window.GenkiExercises = [
   'lesson-10/grammar-wb-4|基礎練習：問題4|p.137',
   'lesson-10/grammar-wb-5|基礎練習：問題5|p.138; A &amp; B',
   'lesson-10/grammar-wb-6|基礎練習：問題6|p.139',
+  'lesson-10/grammar-wb-7|基礎練習：問題7|p.139-140; A &amp; B',
 
   // Appendix
   'appendix/vocabulary-index|単語索引|p.362-398',
