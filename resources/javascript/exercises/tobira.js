976cf6824cbb101d1b5d22bc2c8623cfa8841de9
Seth Clydesdale <sethc1995@gmail.com>
2022-11-25 11:06:41 -0500

    add new exercises
    - added the following exercises.
    
    # 第11課
    - 基礎練習：問題8

--- resources/javascript/exercises/tobira.js
+++ resources/javascript/exercises/tobira.js
@@ -481,6 +481,7 @@ window.GenkiExercises = [
   'lesson-11/grammar-wb-5|基礎練習：問題5|p.153',
   'lesson-11/grammar-wb-6|基礎練習：問題6|p.154',
   'lesson-11/grammar-wb-7|基礎練習：問題7|p.154-155',
+  'lesson-11/grammar-wb-8|基礎練習：問題8|p.155-156; A & B',
 
   // Appendix
   'appendix/vocabulary-index|単語索引|p.362-398',
