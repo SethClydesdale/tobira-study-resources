cd1b0fd632bf3a2f71a2eae1dcab7fad3cadf95c
Seth Clydesdale <sethc1995@gmail.com>
2022-12-29 11:18:22 -0500

    add new exercises
    - added the following exercises.
    
    # 第12課
    - 漢字の練習：問題6
    - 漢字の練習：問題7

--- resources/javascript/exercises/tobira.js
+++ resources/javascript/exercises/tobira.js
@@ -540,6 +540,8 @@ window.GenkiExercises = [
   'lesson-12/kanji-wb-6|漢字の練習：問題3|p.168',
   'lesson-12/kanji-wb-7|漢字の練習：問題4|p.169',
   'lesson-12/kanji-wb-8|漢字の練習：問題5|p.169',
+  'lesson-12/kanji-wb-9|漢字の練習：問題6|p.170',
+  'lesson-12/kanji-wb-10|漢字の練習：問題7|p.170-171',
 
   // Appendix
   'appendix/vocabulary-index|単語索引|p.362-398',
