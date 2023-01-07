723e37d693723a225eada012c4d51fa15c7d58f0
Seth Clydesdale <sethc1995@gmail.com>
2023-01-07 13:08:25 -0500

    add new exercises, definitions
    - added new definitions to the dictionary.
    - added the following exercises.
    
    # 第13課
    - 基礎練習：問題4

--- resources/javascript/exercises/tobira.js
+++ resources/javascript/exercises/tobira.js
@@ -555,6 +555,7 @@ window.GenkiExercises = [
   'lesson-13/grammar-wb-1|基礎練習：問題1|p.171',
   'lesson-13/grammar-wb-2|基礎練習：問題2|p.171',
   'lesson-13/grammar-wb-3|基礎練習：問題3|p.171-172',
+  'lesson-13/grammar-wb-4|基礎練習：問題4|p.172-173',
 
   // Appendix
   'appendix/vocabulary-index|単語索引|p.362-398',
